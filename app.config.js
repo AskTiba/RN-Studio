56099eb2d651f23d5bd08e8c70de8f34db9c1dce
Tibamwenda Anthony <anthonyngisiro@gmail.com>
2024-11-29 04:00:17 +0300

    Update configurations

--- app.config.js
+++ app.config.js
@@ -1,38 +1,8 @@
 import 'dotenv/config';
 
-const IS_DEV = process.env.APP_VARIANT === 'development';
-const IS_PREVIEW = process.env.APP_VARIANT === 'preview';
-
-// Dynamic function to get the unique identifier based on environment
-const getUniqueIdentifier = () => {
-  if (IS_DEV) {
-    return 'com.asktiba.RNStudio.dev';
-  }
-
-  if (IS_PREVIEW) {
-    return 'com.asktiba.RNStudio.preview';
-  }
-
-  return 'com.asktiba.RNStudio';
-};
-
-// Dynamic function to get the app name based on environment
-const getAppName = () => {
-  if (IS_DEV) {
-    return 'RNStudio (Dev)';
-  }
-
-  if (IS_PREVIEW) {
-    return 'RNStudio (Preview)';
-  }
-
-  return 'Studio: Experimentation';
-};
-
 export default {
   expo: {
-    name: getAppName(), // Set app name dynamically based on the environment
-    owner: 'asktiba',
+    name: 'RN-Studio',
     slug: 'RN-Studio',
     version: '1.0.0',
     scheme: 'RN-Studio',
@@ -48,22 +18,16 @@ export default {
     orientation: 'portrait',
     icon: './assets/icon.png',
     userInterfaceStyle: 'light',
-    splash: {
-      image: './assets/splash.png',
-      resizeMode: 'contain',
-      backgroundColor: '#F8F4E1',
-    },
     assetBundlePatterns: ['**/*'],
     ios: {
       supportsTablet: true,
-      bundleIdentifier: getUniqueIdentifier(), // Set the iOS bundleIdentifier dynamically
     },
     android: {
       adaptiveIcon: {
         foregroundImage: './assets/adaptive-icon.png',
-        backgroundColor: '#F8F4E1',
+        backgroundColor: '#ffffff',
       },
-      package: getUniqueIdentifier(), // Set the Android package name dynamically
+      package: 'com.asktiba.RNStudio',
     },
     extra: {
       eas: {
@@ -92,7 +56,7 @@ export default {
         '@rnmapbox/maps',
         {
           RNMapboxMapsVersion: '11.4.0',
-          RNMapboxMapsDownloadToken: process.env.MAPBOX_SECRET_KEY, // Use environment variable for the Mapbox secret key
+          RNMapboxMapsDownloadToken: process.env.MAPBOX_SECRET_KEY, // Uses environment variable here
         },
       ],
       [
